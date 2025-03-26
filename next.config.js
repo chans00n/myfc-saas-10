51a115786bde1ce0239c22a6eadb3086f965349c
Your Name <chanson@stblcreative.com>
2025-03-25 18:44:52 -0700

    fix: add supabase storage domain to next.js image config

--- next.config.js
+++ next.config.js
@@ -48,9 +48,14 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/**',
       }
     ],
-    domains: ['*'],
   },
   experimental: {
     serverActions: {
